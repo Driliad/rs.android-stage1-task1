75da95dd16a1f0b2b738c6861ba3a83fb9491b1a
Dmitry Shkalkov <shkalkov.dima@gmail.com>
2020-03-28 20:02:36 +0300

    feat: implement highestPalindrome task

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -4,6 +4,57 @@ class HighestPalindrome {
 
     // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        var highestPalindrome = digitString.toCharArray();
+        var count = k;
+
+        var start = 0;
+        var end = n - 1;
+
+        var differentNumbers = 0;
+
+        while (start < end) {
+            if (highestPalindrome[start] != highestPalindrome[end]) {
+                val maxValue = maxOf(highestPalindrome[start], highestPalindrome[end]);
+                highestPalindrome[start] = maxValue;
+                highestPalindrome[end] = maxValue;
+
+                count--;
+            }
+
+            start++;
+            end--;
+        }
+
+        if (count < 0) {
+            return "-1";
+        }
+
+        start = 0;
+        end = highestPalindrome.count() - 1;
+
+        while (start <= end) {
+            if (start == end) {
+                if (count > 0) {
+                    highestPalindrome[start] = '9';
+                }
+            }
+
+            if (highestPalindrome[start] < '9') {
+                if (count >= 2 && highestPalindrome[start] == digitString[start] && highestPalindrome[end] == digitString[end]) {
+                    count -= 2;
+                    highestPalindrome[start] = '9';
+                    highestPalindrome[end] = '9';
+                } else if (count >= 1 && (highestPalindrome[start] != digitString[start] || highestPalindrome[end] != digitString[end])) {
+                    count--;
+                    highestPalindrome[start] = '9';
+                    highestPalindrome[end] = '9';
+                }
+            }
+
+            start++;
+            end--;
+        }
+
+        return highestPalindrome.joinToString("");
     }
 }
